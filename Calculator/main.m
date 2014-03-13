eb78a57ae8ee07111946ee0ab8f814068c3c5177
Christos Hrousis <christos.hrousis@gmail.com>
2014-03-13 12:59:44 +1100

    Initial Commit

---
+++ Calculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by AntiFun on 13/03/2014.
+//  Copyright (c) 2014 Think In Pixels. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
